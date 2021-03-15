53efd1fb838a5b8d9fed09b3574725e190e87d57
Kevqn <none>
2021-03-15 22:40:02 +0530

    Previous commit fixes.

--- game/events/start_game.py
+++ game/events/start_game.py
@@ -8,6 +8,6 @@ async def start_game():
     root.bind("<Escape>", pause_game)
     from ..objects.Ship import ship
     from ..objects.Asteroid import START_LOOP
-    START_LOOP()
+    asyncio.ensure_future(START_LOOP())
     canvas.place(x=300,y=100)
     root.lift(canvas)
